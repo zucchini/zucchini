3d18c6505ab3ae146faf914edaea2e5a158dfee2
Austin Adams <git@austinjadams.com>
2017-12-17 19:22:02 -0500

    cli: Add subcommand for running grader in Docker container

--- zucchini/cli.py
+++ zucchini/cli.py
@@ -7,43 +7,53 @@ import click
 
 @click.group()
 def main(args=None):
-    """Command-line interface to zucchini."""
+    """zucchini, a fun autograder for the whole family."""
     pass
 
 
 @main.command()
 def setup():
-    """Prompt for initial global config"""
+    """Prompt for initial global config."""
     pass
 
 
 @main.command()
 def farm():
-    """Add a farm for zucchini configuration"""
+    """Add a farm for zucchini configuration."""
     pass
 
 
 @main.command()
 def list():
-    """Update all farms and list all configurations"""
+    """Update all farms and list all configurations."""
     pass
 
 
 @main.command()
 def init():
-    """Configure a directory for grading"""
+    """Configure a directory for grading."""
     pass
 
 
 @main.command()
 def grade():
-    """Grade submissions"""
+    """Grade submissions."""
+    pass
+
+@main.command('run-grader')
+def run_grader():
+    """
+    Grade the submission held in a directory.
+
+    Useful for grading an individual submission component in a docker
+    container.
+    """
     pass
 
 
 @main.command()
 def export():
-    """Export grades for uploading"""
+    """Export grades for uploading."""
     pass
 
 
