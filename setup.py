b66d4d3ed843091f60273c353e405599c2fd5da7
Austin Adams <git@austinjadams.com>
2018-01-17 12:57:58 -0500

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='zucchini',
-    version='0.1.1',
+    version='0.1.2',
     description="Zucchini is an automatic grader tool for use in grading programming assignments.",
     long_description=readme + '\n\n' + history,
     author="Zucchini Team",
