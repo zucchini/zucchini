13b8478025ffb2ae4eb947611db949bd78d04213
Austin Adams <git@austinjadams.com>
2018-04-15 20:24:33 -0400

    Add CommandGrader

--- zucchini/graders/__init__.py
+++ zucchini/graders/__init__.py
@@ -3,19 +3,21 @@ from .grader_interface import GraderInterface, Part
 from .threaded_grader import ThreadedGrader
 from .prompt_grader import PromptGrader
 from .open_file_grader import OpenFileGrader
+from .command_grader import CommandGrader
 from .libcheck_grader import LibcheckGrader
 from .junit_json_grader import JUnitJSONGrader
 from .junit_xml_grader import JUnitXMLGrader
 from .bitwise_json_grader import BitwiseJSONGrader
 
 __all__ = ['InvalidGraderConfigError', 'GraderInterface', 'Part',
-           'ThreadedGrader', 'PromptGrader', 'OpenFileGrader',
+           'ThreadedGrader', 'PromptGrader', 'OpenFileGrader', 'CommandGrader',
            'LibcheckGrader', 'JUnitJSONGrader', 'JUnitXMLGrader',
            'BitwiseJSONGrader']
 
 _GRADERS = (
     PromptGrader,
     OpenFileGrader,
+    CommandGrader,
     LibcheckGrader,
     JUnitJSONGrader,
     JUnitXMLGrader,
