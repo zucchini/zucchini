d2dff3b05e26ae3c17fdd73b377a1f2a65500803
Henry Bui <henry.h.bui05@gmail.com>
2025-04-06 18:05:29 -0400

    bump 2.2.19

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requirements = [
 
 setup(
     name='zucchini',
-    version='2.2.18',
+    version='2.2.19',
     description="Zucchini is an automatic grader tool for use in grading programming assignments.",
     long_description=readme + '\n\n' + history,
     author="Zucchini Team",
