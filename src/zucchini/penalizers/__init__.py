6016803bdd89bfaa44eb8672c3192b4e8f2be4fa
Henry Bui <henry.h.bui05@gmail.com>
2026-02-05 14:40:13 -0500

    Drop unused penalizer

--- src/zucchini/penalizers/__init__.py
+++ src/zucchini/penalizers/__init__.py
@@ -1,13 +1,11 @@
 from .exceptions import InvalidPenalizerConfigError
 from .penalizer_interface import PenalizerInterface
 from .late_penalizer import LatePenalizer
-from .checkoff_penalizer import CheckoffPenalizer
 
 __all__ = ['InvalidPenalizerConfigError', 'PenalizerInterface',
-           'LatePenalizer', 'CheckoffPenalizer']
+           'LatePenalizer']
 
 _PENALIZERS = (
     LatePenalizer,
-    CheckoffPenalizer
 )
 AVAILABLE_PENALIZERS = {cls.__name__: cls for cls in _PENALIZERS}
