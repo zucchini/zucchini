92ce266a9eacf4f1afbf934d5b89275a7d179109
Henry Bui <henry.h.bui05@gmail.com>
2026-02-07 22:36:35 -0500

    Delete JUnitXMLGrader and PythonModuleGrader

--- src/zucchini/graders/__init__.py
+++ src/zucchini/graders/__init__.py
@@ -1,26 +1,21 @@
 from .exceptions import InvalidGraderConfigError
 from .grader_interface import GraderInterface, Part
 from .threaded_grader import ThreadedGrader
-from .junit_xml_grader import JUnitXMLGrader
 from .bitwise_json_grader import BitwiseJSONGrader
 from .circuitsim_grader import CircuitSimGrader
 from .multi_command_grader import MultiCommandGrader
-from .python_module_grader import PythonModuleGrader
 from .ensemble_grader import EnsembleGrader
 from .criterion_grader import CriterionGrader
 
 __all__ = ['InvalidGraderConfigError', 'GraderInterface', 'Part',
-           'ThreadedGrader','JUnitXMLGrader', 'BitwiseJSONGrader',
-           'CircuitSimGrader', 'MultiCommandGrader', 'PythonModuleGrader',
-           'EnsembleGrader', 'CriterionGrader']
+           'ThreadedGrader', 'BitwiseJSONGrader', 'CircuitSimGrader',
+           'MultiCommandGrader', 'EnsembleGrader', 'CriterionGrader']
 
 _GRADERS = (
     ThreadedGrader,
-    JUnitXMLGrader,
     BitwiseJSONGrader,
     CircuitSimGrader,
     MultiCommandGrader,
-    PythonModuleGrader,
 	EnsembleGrader,
 	CriterionGrader
 )
